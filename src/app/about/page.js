4b0738c853ec9923b68f418ca08840ccda159dc9
Exaucé MARUBA <63616448+Maruba22@users.noreply.github.com>
2025-01-03 19:04:49 +0100

    about page update

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div clasName="max-w-6xl mx-auto space-y-4"> 
+    <div className="max-w-6xl mx-auto space-y-4"> 
         <h1 className="text-2xl font-medium text-amber-600">About</h1>
 
         <p> Welcome to our website to movie video   Welcome to our website to movie video Welcome to our website to movie video Welcome to our website to movie video
