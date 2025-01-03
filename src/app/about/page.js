bdc00373d3f6c072e553eefd616651815dce8f6c
Exaucé MARUBA <63616448+Maruba22@users.noreply.github.com>
2025-01-03 19:03:07 +0100

    create a about page

---
+++ src/app/about/page.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div clasName="max-w-6xl mx-auto space-y-4"> 
+        <h1 className="text-2xl font-medium text-amber-600">About</h1>
+
+        <p> Welcome to our website to movie video   Welcome to our website to movie video Welcome to our website to movie video Welcome to our website to movie video
+        Welcome to our website to movie   Welcome to our website to movie videov Welcome to our website to movie video Welcome to our website to movie video Welcome to our website to movie video
+        video Welcome to our website to movie video Welcome to our website to movie video Welcome to our website to movie video Welcome to our website to movie video Welcome to our website to movie video Welcome to our website to movie video 
+        Welcome to our website to movie   Welcome to our website to movie videov Welcome to our website to movie video Welcome to our website to movie video Welcome to our website to movie video
+        video Welcome to our website to movieWelcome to our website to movie   Welcome to our website to movie videov Welcome to our website to movie video Welcome to our website to movie video Welcome to our website to movie video
+        video Welcome to our website to movie </p>
+        
+
+        <p> Welcome to our website to movie video   Welcome to our website to movie video Welcome to our website to movie video Welcome to our website to movie video
+        Welcome to our website to movie   Welcome to our website to movie videov Welcome to our website to movie video Welcome to our website to movie video Welcome to our website to movie video
+        video Welcome to our website to movie video Welcome to our website to movie video Welcome to our website to movie video Welcome to our website to movie video Welcome to our website to movie video Welcome to our website to movie video 
+        Welcome to our website to movie   Welcome to our website to movie videov Welcome to our website to movie video Welcome to our website to movie video Welcome to our website to movie video
+        video Welcome to our website to movieWelcome to our website to movie   Welcome to our website to movie videov Welcome to our website to movie video Welcome to our website to movie video Welcome to our website to movie video
+        video Welcome to our website to movie </p>
+    </div>
+  )
+}
